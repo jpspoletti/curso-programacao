861c8962bb9ef2a2f6c7f5b4fc7fc97f4b6a73a2
jpspoletti <90663509+jpspoletti@users.noreply.github.com>
2024-06-16 18:56:41 -0300

    Delete curso-programacao directory

--- curso-programacao/A mais/ez.js
+++
@@ -1,6 +0,0 @@
-const numeroAleatorio = gerarNumeroAleatorio();
-console.log(numeroAleatorio);
-
-function gerarNumeroAleatorio() {
-  return Math.floor(Math.random() * 4) + 1;
-}
--- curso-programacao/aula-1/script.js
+++
@@ -1,4 +0,0 @@
-let num = 1
-let num2 = 9
-const soma = num + num2
-console.log(soma)
